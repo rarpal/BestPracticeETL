74594d87dd4579ec5e15db4cb48fa82792a5c6f3
rarpal <rarpal@gmail.com>
2016-02-12 18:18:26 +0000

    office: rejects bug fix

--- ETL/DataWarehouse/audit.tbl_RejectsLog.sql
+++ ETL/DataWarehouse/audit.tbl_RejectsLog.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [audit].[RejectsLog]
 (
 	[LogID] INT NOT NULL , 
-    [TableName] VARCHAR(50) NOT NULL, 
+    [ComponentName] VARCHAR(50) NOT NULL, 
     [KeyValue] VARCHAR(50) NOT NULL, 
     [RowNumber] INT NOT NULL, 
     [Rejects] VARCHAR(1000) NULL, 
     [LogTime] DATETIME NOT NULL, 
-    CONSTRAINT [cix_ReduceLog] PRIMARY KEY ([LogID])
+    CONSTRAINT [cix_ReduceLog] PRIMARY KEY ([LogID], [KeyValue], [RowNumber])
 )
