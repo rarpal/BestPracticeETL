95ca4b7b6ff3dced78c42c030d5bd82d3bbf57b2
rarpal <rarpal@gmail.com>
2016-02-14 22:02:21 +0000

    home: added ETLModel project

---
+++ ETL/ETLModel/AuditRejects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SqlServer.Dts.Pipeline.Wrapper;
+using Microsoft.SqlServer.Dts.Runtime.Wrapper;
+using System.Data.OleDb;
+
+namespace ETLModel
+{
+    public class AuditRejects
+    {
+        public string Property1 {get; set;}
+
+    }
+}
---
+++ ETL/TestProjectETL/UTst.audit.up_CreateRejectsLog.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjectETL
+{
+    [TestClass()]
+    public class UTst : SqlDatabaseTestClass
+    {
+
+        public UTst()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction audit_up_CreateRejectsLogTest_TestAction;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UTst));
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.InconclusiveCondition inconclusiveCondition1;
+            this.audit_up_CreateRejectsLogTestData = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestActions();
+            audit_up_CreateRejectsLogTest_TestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            inconclusiveCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.InconclusiveCondition();
+            // 
+            // audit_up_CreateRejectsLogTestData
+            // 
+            this.audit_up_CreateRejectsLogTestData.PosttestAction = null;
+            this.audit_up_CreateRejectsLogTestData.PretestAction = null;
+            this.audit_up_CreateRejectsLogTestData.TestAction = audit_up_CreateRejectsLogTest_TestAction;
+            // 
+            // audit_up_CreateRejectsLogTest_TestAction
+            // 
+            audit_up_CreateRejectsLogTest_TestAction.Conditions.Add(inconclusiveCondition1);
+            resources.ApplyResources(audit_up_CreateRejectsLogTest_TestAction, "audit_up_CreateRejectsLogTest_TestAction");
+            // 
+            // inconclusiveCondition1
+            // 
+            inconclusiveCondition1.Enabled = true;
+            inconclusiveCondition1.Name = "inconclusiveCondition1";
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+
+        [TestMethod()]
+        public void audit_up_CreateRejectsLogTest()
+        {
+            SqlDatabaseTestActions testActions = this.audit_up_CreateRejectsLogTestData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            try
+            {
+                // Execute the test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+                SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            }
+            finally
+            {
+                // Execute the post-test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+                SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+            }
+        }
+        private SqlDatabaseTestActions audit_up_CreateRejectsLogTestData;
+    }
+}
