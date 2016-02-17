7ef8df42d7d87f5401dabe1749cd8951c2836652
rarpal <rarpal@gmail.com>
2016-02-17 00:44:50 +0000

    home: added audit.ValidationLog

---
+++ ETL/DataWarehouse/audit.tbl_ValidationLog.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [audit].[ValidationLog]
+(
+	[LogID] INT NOT NULL , 
+    [ComponentName] VARCHAR(50) NOT NULL, 
+    [ValidationName] VARCHAR(50) NOT NULL, 
+    [Description] VARCHAR(1000) NULL, 
+    [severity] SMALLINT NOT NULL, 
+    [LogTime] DATETIME NOT NULL
+)
+
+GO
+
+CREATE CLUSTERED INDEX [cix_ValidationLog] ON [audit].[ValidationLog] ([LogID])
